192b5ca98810c93c1568bfd6c29e08caac055501
Caster6443 <dzy5864@gmail.com>
2026-01-20 19:40:02 +0800

    fix: update

--- waybar_config/scripts/get-clock.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-HOUR=$(date "+%H")
-TIME=$(date "+%H:%M")
-TOOLTIP=$(LC_TIME=zh_CN.UTF-8 date "+%Y年%m月%d日 %A")
-ICON=""
-
-printf '{"text": "%s %s", "tooltip": "%s"}\n' "$ICON" "$TIME" "$TOOLTIP"
--- waybar_config/scripts/metadata.sh
+++
@@ -1,3 +0,0 @@
-
-#!/bin/bash
-playerctl metadata --format '{{status_icon}} {{artist}} - {{title}}' 2>/dev/null
